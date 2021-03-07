a435b1f6175f296cd10d0f2d91a2f532027dfe80
Joyamoni <67144093+Joyamoni@users.noreply.github.com>
2021-03-07 22:09:15 +0600

    Update run.py

--- run.py
+++ run.py
@@ -6,29 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index.html')
 def index():
-    return render_template('index.html')
-
-
-@app.route('/addnumber')
-def add():
-    a = request.args.get('a', 0, type=float)
-    b = request.args.get('b', 0, type=float)
-    print(a)
-    return jsonify(result=a + b)
-
-
-@app.route("/test.html")
-def html():
-	return render_template("args.html")
-
-
-@app.route("/req")
-def wow():
-	d=request.args.get("the_data")
-	if d=="hello":
-		return jsonify(msg="wow you clicked",color="red")
-
-
+    return("<h1>Hello flask</h1>")
 
 
 
